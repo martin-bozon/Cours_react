90bf9d88e944f84042a577b3f719575b3d99e875
martin-bozon <martin@krak-n.ai>
2021-09-17 16:51:19 +0200

    Utilisation de useState pour mettre ajour le panier et l'afficher ou le cacher

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,16 @@ import Banner from './Banner';
 import Cart from './Cart';
 import Footer from './Footer';
 import ShoppingList from './ShoppingList';
+import '../styles/Layout.css'
 
 function App() {
   return (    
     <div>      
         <Banner />  
-        {/* <Cart /> */}
-        <ShoppingList />
+        <div className="lmj-layout-inner">
+          <Cart />
+          <ShoppingList />
+        </div>
         <Footer />
     </div>
   )
